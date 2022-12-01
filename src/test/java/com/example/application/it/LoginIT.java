b35f530021a4a29f8bcb1e7abcdd55a8baa8f07f
Petro Ravlinko <petro.ravlinko@gmail.com>
2022-12-02 00:39:49 +0200

    Creating a View Object

--- src/test/java/com/example/application/it/LoginIT.java
+++ src/test/java/com/example/application/it/LoginIT.java
@@ -1,6 +1,6 @@
 package com.example.application.it;
 
-import com.vaadin.flow.component.login.testbench.LoginFormElement;
+import com.example.application.it.elements.LoginViewElement;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,13 +11,13 @@ public class LoginIT extends AbstractTest {
 
     @Test
     public void loginAsValidUserSucceeds() {
-        // Find the LoginForm used on the page
-        LoginFormElement form = $(LoginFormElement.class).first();
-        // Enter the credentials and log in
-        form.getUsernameField().setValue("user");
-        form.getPasswordField().setValue("userpass");
-        form.getSubmitButton().click();
-        // Ensure the login form is no longer visible
-        Assert.assertFalse($(LoginFormElement.class).exists());
+        LoginViewElement loginView = $(LoginViewElement.class).onPage().first();
+        Assert.assertTrue(loginView.login("user", "userpass"));
+    }
+
+    @Test
+    public void loginAsInvalidUserFails() {
+        LoginViewElement loginView = $(LoginViewElement.class).onPage().first();
+        Assert.assertFalse(loginView.login("user", "invalid"));
     }
 }
